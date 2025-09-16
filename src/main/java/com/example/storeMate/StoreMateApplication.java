341c0dec1f5a3668b83627d35c41718dc1c01f0a
kael0595 <changgyu549@gmail.com>
2025-09-16 17:09:01 +0900

    chore: initial commit

---
+++ src/main/java/com/example/storeMate/StoreMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMateApplication.class, args);
+	}
+
+}
