7e82f66ba43b4137b3fd4c11b57680728f5056f9
kael0595 <changgyu549@gmail.com>
2025-09-16 18:19:55 +0900

    feat: Implement JwtProvider to generate and validate JWT tokens

---
+++ src/main/java/com/example/storeMate/base/security/JwtProvider.java
@@ -0,0 +1,64 @@
+package com.example.storeMate.base.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final String secret = "adqoiweukasjdiweuyrksdhkjhsdfiouqweihaskdj";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    private final long expiration = 60 * 60L;
+
+    public String genToken(UserDetails userDetails) {
+
+        Date now = new Date();
+
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaims(token).getSubject();
+    }
+
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        try {
+            String username = getUsernameFromToken(token);
+            return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expirationDate = getClaims(token).getExpiration();
+        return expirationDate.before(new Date());
+    }
+}
