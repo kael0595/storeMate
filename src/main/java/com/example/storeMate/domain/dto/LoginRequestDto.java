e7b230d0cdbfca9a9b375c15ed30e165fc24bc54
kael0595 <changgyu549@gmail.com>
2025-09-17 16:36:50 +0900

    feat: 토큰 발급 및 로그인을 위한 LoginRequestDto 및 LoginRequestDto 구현

---
+++ src/main/java/com/example/storeMate/domain/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.storeMate.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+    private String username;
+
+    private String password;
+}
---
+++ src/main/java/com/example/storeMate/domain/dto/LoginResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.storeMate.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+
+    private String token;
+}
