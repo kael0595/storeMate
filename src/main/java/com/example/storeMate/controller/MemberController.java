21b435eb259dbcc583ef5a1caceebdbc0bf055e2
kael0595 <changgyu549@gmail.com>
2025-09-17 12:03:18 +0900

    feat: 회원 관리를 위한 MemberController 구현 및 register 메서드 추가

---
+++ src/main/java/com/example/storeMate/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.example.storeMate.controller;
+
+import com.example.storeMate.domain.dto.MemberDto;
+import com.example.storeMate.domain.entity.Member;
+import com.example.storeMate.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/register")
+    public ResponseEntity<MemberDto> register(@RequestBody MemberDto memberDto) {
+
+        Member member = memberService.register(memberDto);
+
+        return ResponseEntity.ok().build();
+    }
+}
