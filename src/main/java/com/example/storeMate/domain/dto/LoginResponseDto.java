ad700daf45de9761abdf7987897915379c4082b6
kael0595 <changgyu549@gmail.com>
2025-09-18 17:25:52 +0900

    feat: LoginResponseDto에 refreshToken 필드 추가

--- src/main/java/com/example/storeMate/domain/dto/LoginResponseDto.java
+++ src/main/java/com/example/storeMate/domain/dto/LoginResponseDto.java
@@ -2,10 +2,14 @@ package com.example.storeMate.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDto {
 
     private String token;
+
+    private String refreshToken;
 }
