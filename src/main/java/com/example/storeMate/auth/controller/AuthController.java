56bd45ec6641047163e83275af9777ec9ab07f19
kael0595 <changgyu549@gmail.com>
2025-09-22 23:23:21 +0900

    refactor: login 메서드 MemberController -> AuthController로 이 및 RsData 도입

---
+++ src/main/java/com/example/storeMate/auth/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.storeMate.auth.controller;
+
+import com.example.storeMate.auth.dto.RefreshTokenRequestDto;
+import com.example.storeMate.auth.service.AuthService;
+import com.example.storeMate.base.security.JwtProvider;
+import com.example.storeMate.base.util.rsData.RsData;
+import com.example.storeMate.domain.dto.LoginRequestDto;
+import com.example.storeMate.domain.dto.LoginResponseDto;
+import com.example.storeMate.domain.entity.Member;
+import com.example.storeMate.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    private final MemberService memberService;
+
+    private final JwtProvider jwtProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<RsData<LoginResponseDto>> login(@RequestBody LoginRequestDto loginRequestDto) {
+
+        Member member = memberService.login(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+
+        String accessToken = jwtProvider.genToken(member);
+
+        String refreshToken = authService.genRefreshToken(member);
+
+        LoginResponseDto loginResponseDto = new LoginResponseDto(accessToken, refreshToken);
+
+        return ResponseEntity.ok(new RsData<>("200", "로그인에 성공하였습니다.", loginResponseDto));
+    }
+}
