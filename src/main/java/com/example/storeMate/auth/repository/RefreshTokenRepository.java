8b13a332cbc7b2e29792cfafb9539efb6ea3b570
kael0595 <changgyu549@gmail.com>
2025-09-22 23:29:09 +0900

    feat: RefreshTokenRepository에 findByRefreshToken 메서드 구현

--- src/main/java/com/example/storeMate/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/storeMate/auth/repository/RefreshTokenRepository.java
@@ -4,7 +4,11 @@ import com.example.storeMate.auth.entity.RefreshToken;
 import com.example.storeMate.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     void deleteByMember(Member member);
+
+    Optional<RefreshToken> findByRefreshToken(String token);
 }
