65c3ed4b23e6a396ade247177003a40b8f051c25
kael0595 <changgyu549@gmail.com>
2025-09-22 23:29:44 +0900

    feat: RefreshTokenRequestDto 구현

---
+++ src/main/java/com/example/storeMate/auth/dto/RefreshTokenRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.storeMate.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequestDto {
+    private String refreshToken;
+}
