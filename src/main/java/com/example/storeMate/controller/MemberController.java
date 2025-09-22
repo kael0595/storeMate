f021c631d154c86522fe691329bbaff4f94e8700
kael0595 <changgyu549@gmail.com>
2025-09-22 23:51:27 +0900

    feat: MemberController에 마이페이지 조회용 me 메서드 추가

--- src/main/java/com/example/storeMate/controller/MemberController.java
+++ src/main/java/com/example/storeMate/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.storeMate.controller;
 
+import com.example.storeMate.base.security.JwtProvider;
 import com.example.storeMate.base.util.rsData.RsData;
 import com.example.storeMate.domain.dto.MemberDto;
 import com.example.storeMate.domain.entity.Member;
@@ -15,11 +16,31 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    private final JwtProvider jwtProvider;
+
     @PostMapping("/register")
     public ResponseEntity<RsData> register(@RequestBody MemberDto memberDto) {
 
         Member member = memberService.register(memberDto);
 
         return ResponseEntity.ok(new RsData("200", "회원가입이 완료되었습니다.", member));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<RsData<MemberDto>> me(@RequestHeader("Authorization") String authorizationHeader) {
+
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        String username = jwtProvider.getUsernameFromToken(token);
+
+        Member member = memberService.findByUsername(username);
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setUsername(member.getUsername());
+        memberDto.setEmail(member.getEmail());
+        memberDto.setName(member.getName());
+        memberDto.setRole(member.getRole().getValue());
+
+        return ResponseEntity.ok(new RsData("200", "회원정보 조회에 성공하였습니다.", memberDto));
     }
 }
