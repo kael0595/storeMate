04347cb94118423a289c982d641ce15b67c9c27e
kael0595 <changgyu549@gmail.com>
2025-09-22 23:09:28 +0900

    feat: register 메서드에 RsData 도입

--- src/main/java/com/example/storeMate/controller/MemberController.java
+++ src/main/java/com/example/storeMate/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.storeMate.controller;
 
+import com.example.storeMate.base.util.rsData.RsData;
 import com.example.storeMate.domain.dto.MemberDto;
 import com.example.storeMate.domain.entity.Member;
 import com.example.storeMate.service.MemberService;
@@ -15,10 +16,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<MemberDto> register(@RequestBody MemberDto memberDto) {
+    public ResponseEntity<RsData> register(@RequestBody MemberDto memberDto) {
 
         Member member = memberService.register(memberDto);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new RsData("200", "회원가입이 완료되었습니다.", member));
     }
 }
