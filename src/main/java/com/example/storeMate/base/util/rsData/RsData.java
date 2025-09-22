48fd4da9c24dead4895fad70c7b4c36a244a5460
kael0595 <changgyu549@gmail.com>
2025-09-22 23:02:12 +0900

    feat: API 응답을 위한 RsData 추가

---
+++ src/main/java/com/example/storeMate/base/util/rsData/RsData.java
@@ -0,0 +1,19 @@
+package com.example.storeMate.base.util.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RsData<T> {
+
+    private String statusCode;
+
+    private String massage;
+
+    private T data;
+}
