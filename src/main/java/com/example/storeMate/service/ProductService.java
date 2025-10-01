ae64ec8dc53bd6088b16be0cacf390339750a8d5
kael0595 <changgyu549@gmail.com>
2025-10-02 01:31:54 +0900

    feat: 상품 관리를 위한 ProductService 구현 및 createProduct 메서드 추가

---
+++ src/main/java/com/example/storeMate/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.storeMate.service;
+
+import com.example.storeMate.domain.dto.ProductRequestDto;
+import com.example.storeMate.domain.entity.Product;
+import com.example.storeMate.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product createProduct(ProductRequestDto productRequestDto) {
+
+        int discountPrice = productRequestDto.getPrice() * (100 - productRequestDto.getDiscountRate()) / 100;
+
+        Product product = Product.builder()
+                .name(productRequestDto.getName())
+                .description(productRequestDto.getDescription())
+                .price(productRequestDto.getPrice())
+                .stockQuantity(productRequestDto.getStockQuantity())
+                .status(productRequestDto.getStatus())
+                .category(productRequestDto.getCategory())
+                .brand(productRequestDto.getBrand())
+                .thumbnailImageUrl(productRequestDto.getThumbnailImageUrl())
+                .imageUrls(productRequestDto.getImageUrls())
+                .discountRate(productRequestDto.getDiscountRate())
+                .discountPrice(discountPrice)
+                .isNew(productRequestDto.isNew())
+                .isBest(productRequestDto.isBest())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return productRepository.save(product);
+    }
+}
