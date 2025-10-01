0f3e2255d07980334a735272bfca5df7d9fc8f5c
kael0595 <changgyu549@gmail.com>
2025-10-02 01:25:18 +0900

    feat: Product 엔티티에 validation 추가

--- src/main/java/com/example/storeMate/domain/entity/Product.java
+++ src/main/java/com/example/storeMate/domain/entity/Product.java
@@ -2,6 +2,8 @@ package com.example.storeMate.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -20,34 +22,48 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String description;
 
+    @Column(nullable = false)
     private int price;
 
+    @Column(nullable = false)
     private int stockQuantity;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private ProductStatus status;
 
+    @Column(nullable = false)
     private String category;
 
+    @Column(nullable = false)
     private String brand;
 
     private String thumbnailImageUrl;
 
+    @ElementCollection
     private List<String> imageUrls;
 
+    @Column(nullable = false)
     private int discountRate;
 
+    @Column(nullable = false)
     private int discountPrice;
 
+    @Column(nullable = false)
     private boolean isNew;
 
+    @Column(nullable = false)
     private boolean isBest;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
