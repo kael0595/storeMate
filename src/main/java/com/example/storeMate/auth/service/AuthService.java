09f6a41b61fe45828b199f6c857fcc8574bef4d5
kael0595 <changgyu549@gmail.com>
2025-10-22 14:19:21 +0900

    feat: AuthorizeHeader를 통한 로그인한 회원 정보 찾는 메서드 추가

--- src/main/java/com/example/storeMate/auth/service/AuthService.java
+++ src/main/java/com/example/storeMate/auth/service/AuthService.java
@@ -2,7 +2,10 @@ package com.example.storeMate.auth.service;
 
 import com.example.storeMate.auth.entity.RefreshToken;
 import com.example.storeMate.auth.repository.RefreshTokenRepository;
+import com.example.storeMate.base.security.JwtProvider;
 import com.example.storeMate.domain.entity.Member;
+import com.example.storeMate.repository.MemberRepository;
+import com.example.storeMate.service.MemberService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,10 @@ import java.util.UUID;
 public class AuthService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+
+    private final JwtProvider jwtProvider;
+
+    private final MemberService memberService;
 
     @Transactional
     public void logout(Member member) {
@@ -38,5 +45,15 @@ public class AuthService {
         RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(token)
                 .orElseThrow(() -> new RuntimeException("RefreshToken이 존재하지 않습니다."));
         return refreshToken.getMember();
+    }
+
+
+    public Member getMemberFromAuthorizationHeader(String authorizationHeader) {
+
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        String username = jwtProvider.getUsernameFromToken(token);
+
+        return memberService.findByUsername(username);
     }
 }
